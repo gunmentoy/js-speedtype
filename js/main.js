794b1c430236981a45257fa5508cd7a4d4b15a70
gunmentoy <tomnguyen0443@gmail.com>
2023-11-17 14:08:32 -0800

    added the index.html file

---
+++ js/main.js
@@ -0,0 +1,110 @@
+window.addEventListener("load", init);
+
+// global variables
+let time = 3;
+let score = 0;
+let isPlaying;
+
+// DOM elements
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
+
+// words array
+const words = [
+  "hat",
+  "river",
+  "lucky",
+  "statue",
+  "generate",
+  "stubborn",
+  "cocktail",
+  "runaway",
+  "joke",
+  "developer",
+  "establishment",
+  "hero",
+  "javascript",
+  "nutrition",
+  "revolver",
+  "echo",
+  "siblings",
+  "investigate",
+  "horrendous",
+  "symptom",
+  "laughter",
+  "magic",
+  "master",
+  "space",
+  "definition",
+  "apple",
+  "banana",
+  "chocolate",
+  "elephant",
+  "fireworks",
+  "guitar",
+  "happiness",
+  "island",
+  "jungle",
+  "kangaroo",
+  "lemon",
+  "mountain",
+  "nightmare",
+  "ocean",
+  "piano",
+  "queen",
+  "rainbow",
+  "sunset",
+  "tiger",
+  "umbrella",
+  "volcano",
+  "waterfall",
+  "xylophone",
+  "yoga",
+  "zebra"
+];
+
+// initialize game
+function init() {
+  // load word from array
+  showWord(words);
+  // start matching on word input
+  wordInput.addEventListener("input", startMatch);
+  // call countdown every second
+  setInterval(countdown, 1000);
+  // check game status
+  setInterval(checkStatus, 50);
+}
+
+// pick and show random word
+function showWord(words) {
+  // generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+  // output random word
+  currentWord.innerHTML = words[randIndex];
+}
+
+// countdown timer
+function countdown() {
+  // make sure time is not run out
+  if (time > 0) {
+    // decrement
+    time--;
+  } else if (time === 0) {
+    // game is over
+    isPlaying = false;
+  }
+  // show time
+  timeDisplay.innerHTML = time;
+}
+
+// check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "Game Over!";
+    score = -1;
+  }
+}
