617a03882bae52cebbd8d84c8326f5ba6fac3128
gunmentoy <tomnguyen0443@gmail.com>
2023-11-17 14:51:18 -0800

    added functions that changes game mode

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-window.addEventListener("load", init);
-
-// global variables
-// available levels
+// Global variables
 const levels = {
   easy: 5,
   medium: 3,
   hard: 2,
 };
-
-// to change level
-const currentLevel = levels.easy;
-
+let currentLevel = levels.easy;
 let time = currentLevel;
 let score = 0;
 let isPlaying;
@@ -23,128 +17,79 @@ const timeDisplay = document.querySelector("#time");
 const message = document.querySelector("#message");
 const seconds = document.querySelector("#seconds");
 
-// words array
+// Words array
 const words = [
-  "hat",
-  "river",
-  "lucky",
-  "statue",
-  "generate",
-  "stubborn",
-  "cocktail",
-  "runaway",
-  "joke",
-  "developer",
-  "establishment",
-  "hero",
-  "javascript",
-  "nutrition",
-  "revolver",
-  "echo",
-  "siblings",
-  "investigate",
-  "horrendous",
-  "symptom",
-  "laughter",
-  "magic",
-  "master",
-  "space",
-  "definition",
-  "apple",
-  "banana",
-  "chocolate",
-  "elephant",
-  "fireworks",
-  "guitar",
-  "happiness",
-  "island",
-  "jungle",
-  "kangaroo",
-  "lemon",
-  "mountain",
-  "nightmare",
-  "ocean",
-  "piano",
-  "queen",
-  "rainbow",
-  "sunset",
-  "tiger",
-  "umbrella",
-  "volcano",
-  "waterfall",
-  "xylophone",
-  "yoga",
-  "zebra",
+  "hat", "river", "lucky", "statue", "generate", "stubborn", "cocktail", "runaway", "joke", "developer",
+  "establishment", "hero", "javascript", "nutrition", "revolver", "echo", "siblings", "investigate", "horrendous",
+  "symptom", "laughter", "magic", "master", "space", "definition", "apple", "banana", "chocolate", "elephant",
+  "fireworks", "guitar", "happiness", "island", "jungle", "kangaroo", "lemon", "mountain", "nightmare", "ocean",
+  "piano", "queen", "rainbow", "sunset", "tiger", "umbrella", "volcano", "waterfall", "xylophone", "yoga", "zebra"
 ];
 
-// initialize game
+// Initialize game
 function init() {
-  // show number of seconds in UI
   seconds.innerHTML = currentLevel;
-  // load word from array
-  showWord(words);
-  // start matching on word input
+  showWord();
   wordInput.addEventListener("input", startMatch);
-  // call countdown every second
   setInterval(countdown, 1000);
-  // check game status
   setInterval(checkStatus, 50);
 }
 
-// start matching words
+// Start matching words
 function startMatch() {
   if (matchWords()) {
     isPlaying = true;
     time = currentLevel + 1;
-    showWord(words);
+    showWord();
     wordInput.value = "";
     score++;
   }
-  // if score is -1, display 0
-  if (score === -1) {
-    scoreDisplay.innerHTML = 0;
-  } else {
-    scoreDisplay.innerHTML = score;
-  }
+  scoreDisplay.innerHTML = score === -1 ? 0 : score;
 }
 
-// match currentWord to wordInput
+// Match currentWord to wordInput
 function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = "correct!";
-    return true;
-  } else {
-    message.innerHTML = "";
-    return false;
-  }
+  const isCorrect = wordInput.value === currentWord.innerHTML;
+  message.innerHTML = isCorrect ? "correct!" : "";
+  return isCorrect;
 }
 
-// pick and show random word
-function showWord(words) {
-  // generate random array index
+// Pick and show random word
+function showWord() {
   const randIndex = Math.floor(Math.random() * words.length);
-  // output random word
   currentWord.innerHTML = words[randIndex];
 }
 
-// countdown timer
+// Countdown timer
 function countdown() {
-  // make sure time is not run out
   if (time > 0) {
-    // decrement
     time--;
   } else if (time === 0) {
-    // game is over
     isPlaying = false;
   }
-  // show time
   timeDisplay.innerHTML = time;
 }
 
-// check game status
+// Check game status
 function checkStatus() {
   if (!isPlaying && time === 0) {
     message.innerHTML = "game over :( hahah loser!!";
     score = -1;
   }
 }
+
+// Set game mode
+function setGameMode(mode) {
+  currentLevel = levels[mode];
+  time = currentLevel;
+  seconds.innerHTML = currentLevel;
+  score = 0;
+  scoreDisplay.innerHTML = score;
+  isPlaying = false;
+}
+
+// Event listeners
+window.addEventListener("load", init);
+document.getElementById("easy").addEventListener("click", () => setGameMode("easy"));
+document.getElementById("medium").addEventListener("click", () => setGameMode("medium"));
+document.getElementById("hard").addEventListener("click", () => setGameMode("hard"));
